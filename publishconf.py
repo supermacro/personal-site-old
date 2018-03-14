2c8825b1b4baaf42807b3c9105c2fb87c87ed861
Giorgio Delgado <hi@gdelgado.ca>
2018-03-13 20:13:03 -0400

    update siteurl

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://gdelgado14.gitlab.io/gDelgado14.gitlab.io/'
+SITEURL = 'https://gdelgado14.gitlab.io/'
 RELATIVE_URLS = False
 
 PAGE_URL = 'pages/{slug}.html'
